ba7bb67a82a3777e1a2b6bbeb92a0d96fe209f71
Philip Keller <philipjoelkeller@gmail.com>
2023-03-14 14:21:49 -0700

    Initial Commit

---
+++ TapCount/TapCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCountApp.swift
+//  TapCount
+//
+//  Created by Philip Keller on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TapCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
